9cac4a087d7d14d27e9a99ce09f1d2480cdacf2e
Yucheng Low <2740522+ylow@users.noreply.github.com>
2024-12-11 11:15:14 -0800

    fun generator version

---
+++ 11/generator1.py
@@ -0,0 +1,27 @@
+a = [int(i) for i in open('in.txt').read().strip().split()]
+
+
+def stone_generator(stone, step):
+    if step == 0:
+        yield stone
+    elif stone == 0:
+        yield from stone_generator(1, step - 1)
+    elif len(str(stone)) % 2 == 0:
+        z = str(stone)
+        k = len(z) // 2
+        yield from stone_generator(int(z[:k]), step - 1)
+        yield from stone_generator(int(z[k:]), step - 1)
+    else:
+        yield from stone_generator(stone * 2024, step - 1)
+
+
+def generate_all(stonelist, step):
+    for i in stonelist:
+        yield from stone_generator(i, step)
+
+
+nsteps = 25
+ctr = 0
+for i in generate_all(a, nsteps):
+    ctr += 1
+print(ctr)
